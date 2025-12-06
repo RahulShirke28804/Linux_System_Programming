dbd52c45af7992621eeda2aab2614d9c4b0563f6
RahulShirke28804 <rahulshirke28804@gmail.com>
2025-12-06 18:23:02 +0530

    Add files via upload

---
+++ open/open_5.c
@@ -0,0 +1,22 @@
+#include <fcntl.h>
+#include<stdio.h>
+#include <unistd.h>
+
+int main()
+{
+    int fd = 0;
+    fd = open("Second.c",O_CREAT | O_TRUNC,0777);
+
+    if(fd == -1)
+    {
+        printf("Unable to create file\n");
+    }
+    else
+    {
+        printf("File gets successfully created with fd %d\n",fd);
+    }
+
+    close(fd);
+
+    return 0;
+}
