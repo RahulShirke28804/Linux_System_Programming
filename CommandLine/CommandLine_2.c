9c8a86dc00b79408541293d4a04d04cc36534c56
RahulShirke28804 <rahulshirke28804@gmail.com>
2026-01-07 14:05:19 +0530

    Document command-line argument handling in C program
    Add comments to explain command-line argument usage.

--- CommandLine/CommandLine_2.c
+++ CommandLine/CommandLine_2.c
@@ -1,3 +1,8 @@
+/*
+    This C program illustrates how command-line arguments are accessed using argc and argv. 
+    It prints the total number of arguments passed to the program and then displays the 
+    executable name along with the first two command-line arguments.
+*/
 #include<stdio.h>
 
 int main(int argc, char *argv[])
