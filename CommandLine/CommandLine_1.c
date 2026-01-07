328a8787fea1f460252f304b464ad6cf15e854ab
RahulShirke28804 <rahulshirke28804@gmail.com>
2026-01-07 14:04:21 +0530

    Update CommandLine_1.c

--- CommandLine/CommandLine_1.c
+++ CommandLine/CommandLine_1.c
@@ -1,3 +1,8 @@
+/*
+    This C program demonstrates the use of command-line arguments. 
+    It prints the total number of arguments passed to the program (argc) 
+    and displays the program’s executable name using argv[0].
+*/
 #include<stdio.h>
 
 int main(int argc, char *argv[])
