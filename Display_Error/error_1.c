0f921fced297e8478651f66a9ac5c83d4166e643
RahulShirke28804 <rahulshirke28804@gmail.com>
2026-01-07 14:09:05 +0530

    Update error_1.c

--- Display_Error/error_1.c
+++ Display_Error/error_1.c
@@ -1,3 +1,8 @@
+/*
+    This C program demonstrates the use of the global variable errno. 
+    It simply prints the current value of errno to show how the error 
+    indicator is stored and accessed in C programs.
+*/
 #include<stdio.h>
 #include<errno.h>
 
