61978d89ae7e4651af3ad8d4694473b2cdfeee66
RahulShirke28804 <rahulshirke28804@gmail.com>
2026-01-07 14:14:57 +0530

    Update creat_1.c

--- create/creat_1.c
+++ create/creat_1.c
@@ -1,3 +1,8 @@
+/*
+    This C program demonstrates basic file creation using the creat() system call. 
+    It attempts to create a file named LSP.txt with full permissions (0777) and 
+    prints whether the operation was successful along with the returned file descriptor.
+*/
 #include<fcntl.h>
 #include<stdio.h>
 
