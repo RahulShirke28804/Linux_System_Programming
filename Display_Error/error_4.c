54483daed3f513f3d0861f743c5fcefaf6d589d2
RahulShirke28804 <rahulshirke28804@gmail.com>
2026-01-07 14:11:16 +0530

    Update error_4.c

--- Display_Error/error_4.c
+++ Display_Error/error_4.c
@@ -1,3 +1,8 @@
+/*
+    This C program shows safe handling of errno. It attempts to open Demo.txt in read-only mode
+    and prints the returned file descriptor. If an error occurs (i.e., errno is non-zero), 
+    it prints both the numeric error value and the descriptive error message.
+*/
 #include<stdio.h>
 #include<errno.h>
 #include<fcntl.h>
